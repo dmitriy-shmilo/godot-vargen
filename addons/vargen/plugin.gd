9a9991cd3913f39d803363c28cb61ffbb1da7e83
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-06-21 07:56:46 +0300

    Show a warning message when generation is impossible.

--- addons/vargen/plugin.gd
+++ addons/vargen/plugin.gd
@@ -1,13 +1,20 @@
 tool
 extends EditorPlugin
 
+const DOCK_SCENE = preload("res://addons/vargen/vargen_dock.tscn")
 const ONREADY_BLOCK_START = "\nonready "
 
-var _dock: VargenDock
+var _dock
 var _preferences: VargenPreferences
+var _selection: EditorSelection
+var _interface: EditorInterface
 
 func _enter_tree() -> void:
-	_dock = load("res://addons/vargen/vargen_dock.tscn").instance()
+	_interface = get_editor_interface()
+	_selection = _interface.get_selection()
+	_selection.connect("selection_changed", self, "_on_selection_changed")
+
+	_dock = DOCK_SCENE.instance()
 	_dock.connect("run_pressed", self, "_on_run_pressed")
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, _dock)
 	_load_preferences()
@@ -15,36 +22,29 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	remove_control_from_docks(_dock)
+	if _preferences != null:
+		_preferences = null
+		_preferences.free()
+
 	if _dock != null:
-		_dock.queue_free()
+		_dock = null
+		_dock.free()
+
+
+func _on_selection_changed() -> void:
+	_dock.set_is_run_enabled(true, "")
 
 
 func _on_run_pressed(sender: VargenDock, options: Dictionary) -> void:
 	_save_preferences(options)
 
-	var interface = get_editor_interface()
-	var root_node = interface.get_edited_scene_root()
-	if root_node == null:
-		printerr("VarGen: No root node found. Try saving your scene first.")
+	var root_node = _interface.get_edited_scene_root()
+	var nodes = _selection.get_selected_nodes()
+
+	if not _validate_selection(root_node, nodes):
 		return
 
 	var script = root_node.get_script()
-
-	if script == null:
-		printerr("VarGen: Current scene root doesn't have a script attached.")
-		return
-
-	if not script is GDScript:
-		printerr("VarGen: Scene root script is not a GDScript.")
-		return
-
-	var selection = interface.get_selection()
-	var nodes = selection.get_selected_nodes()
-
-	if nodes.size() == 0:
-		printerr("VarGen: No nodes selected in the scene explorer.")
-		return
-
 	var source_code = script.source_code
 	var insert_position = _find_insert_position(source_code)
 
@@ -57,10 +57,10 @@ func _on_run_pressed(sender: VargenDock, options: Dictionary) -> void:
 	file.store_string(source_code)
 	file.close()
 	script.source_code = source_code
-	interface.set_main_screen_editor("Script")
 
-	interface.edit_script(script)
-	interface.get_script_editor()._reload_scripts()
+	_interface.set_main_screen_editor("Script")
+	_interface.edit_script(script)
+	_interface.get_script_editor()._reload_scripts()
 
 
 func _find_insert_position(source_code: String) -> int:
@@ -89,3 +89,24 @@ func _save_preferences(options: Dictionary) -> void:
 func _load_preferences() -> void:
 	_preferences = ResourceLoader.load("res://addons/vargen/vargen_preferences.tres") as VargenPreferences
 	_dock.field_prefix = _preferences.field_prefix
+
+
+func _validate_selection(root_node: Node, selected_nodes: Array) -> bool:
+	if root_node == null:
+		_dock.set_is_run_enabled(false, "No root node found. Try saving your scene first.")
+		return false
+
+	var root_script = root_node.get_script()
+	if root_script == null:
+		_dock.set_is_run_enabled(false, "Current scene root doesn't have a script attached.")
+		return false
+
+	if not root_script is GDScript:
+		_dock.set_is_run_enabled(false, "Scene root script is not a GDScript.")
+		return false
+
+	if selected_nodes.size() == 0:
+		_dock.set_is_run_enabled(false, "No nodes selected in the scene explorer.")
+		return false
+
+	return true
--- addons/vargen/vargen_dock.gd
+++ addons/vargen/vargen_dock.gd
@@ -9,14 +9,28 @@ signal run_pressed(sender, options)
 const INDICATOR_COLLAPSED = preload("res://addons/vargen/icon_GUI_tree_arrow_right.svg")
 const INDICATOR_REVEALED = preload("res://addons/vargen/icon_GUI_tree_arrow_down.svg")
 
+onready var _warning_label: Label = $"WarningLabel"
+onready var _run_button: Button = $"RunButton"
 onready var _collapse_options_indicator: TextureRect = $"HBoxContainer/Indicator"
 onready var _options_container: Control = $"OptionsContainer"
 onready var _prefix_edit: LineEdit = $"OptionsContainer/HBoxContainer/PrefixEdit"
 
 var _is_collapsed: bool = true setget _set_is_collapsed
+var _is_run_enabled: bool = false
+var _warning_message: String = ""
 
 func set_field_prefix(val: String) -> void:
+	field_prefix = val
 	_prefix_edit.text = val
+
+
+func set_is_run_enabled(enabled: bool, reason: String) -> void:
+	_is_run_enabled = enabled
+	_warning_message = reason
+	if is_inside_tree():
+		_warning_label.visible = !reason.empty()
+		_run_button.disabled = !enabled
+		_warning_label.text = reason
 
 
 func _set_is_collapsed(val: bool) -> void:
